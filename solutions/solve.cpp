0b073c2c061c83ea0474944d1090188b64628471
Coolewreyboy <bukind28@gmail.com>
2025-03-15 00:01:07 +0700

    Add files via upload

---
+++ solutions/solve.cpp
@@ -0,0 +1,89 @@
+//Cool_ewrey_boy
+
+#include <bits/stdc++.h>
+
+#define size(v) (int) v.size()
+#define all(v) begin(v), end(v)
+#define ff(T, name, args...) function<T(args)> name = [&](args)->T
+#define jj(v...) v; [](auto&...x){(cin >> ... >> x);}(v);
+#if !LOCAL
+   #define endl '\n'
+#endif
+
+using namespace std;
+using ll = decltype(1LL);
+const ll INF = 1e18l + 100;
+const int mod = 1e9 + 7, inf = 1e9 + 100;
+
+template<int MOD>
+struct modint {
+    int x;
+    modint() { x = 0; }
+    modint(int xx) {
+        if(x = xx; x < 0 || x >= MOD) x %= MOD;
+        if(x < 0) x += MOD;
+    }
+    modint(ll xx) { if(x = xx % MOD; x < 0) x += MOD; }
+    #define op(A, B, C) modint &operator A(const modint &b) { C return *this; } friend modint operator B(modint a, const modint &b) { return a A b; }
+    op(+=, +, x += b.x; if(x >= MOD) x %= MOD;)
+    op(-=, -, x -= b.x; if(x < 0) x += MOD;)
+    op(*=, *, x = (long long) x * b.x % MOD;)
+    modint &operator/=(const modint &b) {
+        int xx = 1, yy = 0, aa = MOD, bb = b.x;
+        while(bb) swap(xx, yy -= xx * (aa / bb)), swap(bb, aa %= bb);
+        if(xx % MOD == 0) swap(xx, yy);
+        if(x = (long long) x * xx % MOD; x < 0 || x >= MOD) x %= MOD;
+        if(x < 0) x += MOD;
+        return *this;
+    }
+    friend modint operator/(modint a, const modint &b) { return a /= b; }
+    friend modint pow(modint a, long long n) {
+        if(n < 0) return pow(modint(1) / a, MOD - 1 ?  MOD - 1 - n % (MOD - 1): 0);
+        if(!n) return modint(1);
+        if(n & 1) return a * pow(a, n - 1);
+        return pow(a * a, n >> 1);
+    }
+    modint &operator-() {
+      x = -x;
+      if(x != 0) x += MOD;
+      return *this;
+    }
+    friend bool operator==(const modint &a, const modint &b) { return a.x == b.x; }
+    friend ostream &operator<<(ostream &s, const modint &b) { return s << b.x; }
+};
+using mint = modint<(int) 1e9>; 
+
+template<class T>
+struct fibonacci {
+   T operator()(long long n) {
+      T x(0), y(1);
+      bool sign = n < 0;
+      n = abs(n);
+      for(int i = (n ? __lg(n): 0); i >= 0; i--) {
+         T xx = x * x, yy = y * y;
+         x *= y + y - x, y = xx + yy;
+         if((n >> i) & 1) swap(y, x += y);
+      }
+      if(!sign || n & 1) return x;
+      return -x;
+   }
+   T gcd(long long i, long long j) {
+      return operator()(std::gcd(i, j));
+   }
+};
+fibonacci<mint> fib;
+
+void solve(int __case) {
+   ll jj(n);
+   cout << fib(n) << endl;
+}
+
+int main() {
+   cin.tie(0)->sync_with_stdio(0);
+   if(auto s = "in.txt"; fopen(s, "r") && freopen(s, "r", stdin));
+   int t = 1;
+   //cin >> t;
+   //t = 1;
+   while(t--) solve(t);
+   return 0;
+}
