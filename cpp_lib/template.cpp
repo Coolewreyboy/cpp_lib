18d31496a36dd07a1caf4f9d1e3c85762cf6cd3f
Coolewreyboy <bukind28@gmail.com>
2025-03-15 00:03:09 +0700

    Add files via upload

---
+++ cpp_lib/template.cpp
@@ -0,0 +1,30 @@
+//Cool_ewrey_boy
+
+#include <bits/stdc++.h>
+
+#define size(v) (int) v.size()
+#define all(v) begin(v), end(v)
+#define ff(T, name, args...) function<T(args)> name = [&](args)->T
+#define jj(v...) v; [](auto&...x){(cin >> ... >> x);}(v);
+#if !LOCAL
+   #define endl '\n'
+#endif
+
+using namespace std;
+using ll = decltype(1LL);
+const ll INF = 1e18l + 100;
+const int mod = 1e9 + 7, inf = 1e9 + 100;
+
+void solve(int __case) {
+   //...//
+}
+
+int main() {
+   cin.tie(0)->sync_with_stdio(0);
+   if(auto s = "in.txt"; fopen(s, "r") && freopen(s, "r", stdin));
+   int t = 1;
+   //cin >> t;
+   //t = 1;
+   while(t--) solve(t);
+   return 0;
+}
