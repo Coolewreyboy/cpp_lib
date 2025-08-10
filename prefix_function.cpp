9a564550587250943c31432c70d13cff9aac881a
Coolewreyboy <bukind28@gmail.com>
2025-08-10 18:41:41 +0700

    Update prefix_function.cpp

--- prefix_function.cpp
+++ prefix_function.cpp
@@ -1,11 +1,11 @@
-vector<int> prefix_function(auto begin, auto end)
+std::vector<int> prefix_function(auto begin, auto end)
 {
-	vector<int> p(end - begin);
-	for(auto ptr = begin + 1; ptr < end; ptr++) {
-		int k = p[ptr - begin - 1];
-		while(k > 0 && *(begin + k) != *ptr)
-			k = p[k - 1];
-		p[ptr - begin] = k + (*ptr == *(begin + k));
-	}
-	return p;
+    std::vector<int> p(end - begin);
+    for(auto ptr = begin + 1; ptr < end; ptr++) {
+        int k = p[ptr - begin - 1];
+        while(k > 0 && *(begin + k) != *ptr)
+            k = p[k - 1];
+        p[ptr - begin] = k + (*ptr == *(begin + k));
+    }
+    return p;
 }
