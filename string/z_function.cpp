ae49654d81498fb030f6fa82e94b60cabf98c3e4
Coolewreyboy <bukind28@gmail.com>
2025-08-13 13:18:45 +0700

    Update and rename z_algorithm.cpp to z_function.cpp

--- string/z_function.cpp
+++ string/z_function.cpp
@@ -1,30 +1,13 @@
-#include <bits/stdc++.h>
-
-#define all(v) begin(v), end(v)
-
-using namespace std;
-
 std::vector<int> z_function(auto begin, auto end)
 {
     std::vector<int> z(end - begin);
     int n = end - begin;
     for(int i = 1, j = 1; i < n; i++) {
-        if(j + z[j] > i)
-		z[i] = min(z[i - j], j + z[j] - i);
+        z[i] = min(z[i - j], max(0, j + z[j] - i));
         while(i + z[i] < n && *(begin + z[i]) == *(begin + i + z[i]))
             z[i]++;
         if(i + z[i] > j + z[j])
             j = i;
-    }	
+    }
     return z;
-}
-
-int main()
-{
-	cin.tie(0)->sync_with_stdio(0);
-	string s;
-	cin >> s;
-	for(auto i : z_function(all(s)))
-		cout << i << ' ';
-	return 0;
 }
