29dedccec437fd3d1c953e56e260d35718af2e27
Coolewreyboy <bukind28@gmail.com>
2025-08-22 20:04:01 +0700

    Update z_function.cpp

--- string/z_function.cpp
+++ string/z_function.cpp
@@ -1,7 +1,7 @@
 std::vector<int> z_function(auto begin, auto end)
 {
-    std::vector<int> z(end - begin);
     int n = end - begin;
+    std::vector<int> z(n);
     for(int i = 1, j = 1; i < n; i++) {
         z[i] = min(z[i - j], max(0, j + z[j] - i));
         while(i + z[i] < n && *(begin + z[i]) == *(begin + i + z[i]))
