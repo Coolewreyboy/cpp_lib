57defeffa7bc2ba4a768ff869a451a788329e235
Coolewreyboy <bukind28@gmail.com>
2025-08-24 01:54:06 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,7 +6,7 @@
 #define all(v) begin(v), end(v)
 #define ff(T, name, args...) function<T(args)> name = [&](args)->T
 #if !LOCAL
-   #define endl '\n'
+    #define endl '\n'
 #endif
 
 using namespace std;
@@ -16,16 +16,17 @@ const int mod = 1e9 + 7, inf = 1e9 + 100;
 
 void solve(int __case)
 {
-   //...//
+    //...//
 }
 
-int main() {
-   cin.tie(0)->sync_with_stdio(0);
-   if(auto s = "in.txt"; fopen(s, "r") && freopen(s, "r", stdin)){};
-   int t = 1;
-   //cin >> t;
-   //t = 1;
-   for(int T = 1; T < t; T++)
-      solve(T);
-   return 0;
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    if(auto s = "in.txt"; fopen(s, "r") && freopen(s, "r", stdin));
+    int t = 1;
+    //cin >> t;
+    //t = 1;
+    for(int T = 1; T <= t; T++)
+        solve(T);
+    return 0;
 }
